338bfbbbb26f05d8bebb714e219dc0e8c714da3e
baymax <baymax-wsl>
2024-10-28 00:09:34 +0200

    WIP SNTP

---
+++ src/libs/SNTPClient/SNTPClient.cpp
@@ -0,0 +1,179 @@
+#include "SNTPClient.h"
+
+// singleton
+SNTPClient& SNTPClient::getInstance()
+{
+    static SNTPClient instance;
+    return instance;
+}
+
+// const and destuct
+SNTPClient::SNTPClient(){}
+SNTPClient::~SNTPClient(){}
+
+/** 
+ * @brief call to initialize the sntp client
+ * @param udp_inst instance pointing to the UDP object. caller is responsible for creating the object
+ * @param time_offset time zone diff
+*/
+void SNTPClient::init(UDP* udp_inst, const char* server_name, uint64_t time_offset)
+{
+    // pointer patrol
+    if(udp_inst == nullptr)
+        return;
+    
+    if(server_name == nullptr)
+        return;
+
+    _udp = udp_inst;
+    strncpy(_pool_name, server_name, SNTPC_SERVER_NAME_MAX_LEN);
+    
+    // flag up
+    _initialized = true;
+}
+
+/**
+ * @brief call to set the server name
+ * @param name domain name of the ntp server
+ */
+void SNTPClient::setServerName(const char* name)
+{
+    // pointer patrol
+    if(name == nullptr)
+        return;
+
+    strncpy(_pool_name, name, SNTPC_SERVER_NAME_MAX_LEN);
+    _use_ip = false;
+}
+
+/**
+ * @brief call to set the server IP
+ * @param ip IP address of the ntp server
+ */
+void SNTPClient::setServerIP(IPAddress ip)
+{
+    // pointer patrol
+    if(name == nullptr)
+        return;
+
+    _pool_ip = ip;
+    _use_ip = true;
+}
+
+
+/**
+ * @brief call to sync time from the ntp server, this is a state machine which can be repeatedly called. the sm initiates a udp request
+ * to the server to sync time 
+ * @returns true on success. It's recommended to read the epoch before the next call to getNTPTime
+ */
+bool SNTPClient::getNTPtime()
+{
+
+    switch(_sm_state)
+    {
+        // to be used in sm sub states
+        int ret = 0;
+
+        case sntpc_sm_init:
+            // reset
+            memset(_data_out.raw, 0, sizeof(ntp_packet_t));
+
+            // setup ntp v4
+            _data_out.ntp_pkt.li = 3;           // unknown
+            _data_out.ntp_pkt.vn = 4            // v4
+            _data_out.ntp_pkt.mode = 3;         // client
+            _data_out.ntp_pkt.stratum = 0;      // unspecified
+            _data_out.ntp_pkt.poll = 6;         // speced minimum
+            _data_out.ntp_pkt.precision = -20;  // 2^-20 ~ 1us precision
+
+            // setup a sick message in refID, refID is typically used for clock identification, we identify as SICK, but the server will
+            // most likely respond with it's ip, on rare occasions we might(never) get GOES
+            _data_out.ntp_pkt.ref_Id = 0x5349434B
+
+            _fcount = 0;
+
+            // advance
+            _sm_state = sntpc_sm_flush_udp;
+            break;
+            
+        // flush out the udp buffers
+        case sntpc_sm_flush_udp:
+            if(_udp->parsePacket() != 0)
+            {
+                _udp->flush();
+                return false;
+            }
+
+            // advance
+            _sm_state = sntpc_sm_begin_pkt;
+            break;
+
+        // this will establish a udp stream on the speced port and name, if unsuccessfull something went wrong
+        case sntpc_sm_begin_pkt:
+            ret = _udp->beginPacket(_use_ip? _pool_ip : _pool_name, SNTPC_PORT);
+            if(ret == 0)
+            {
+                // failed reset to init
+                _sm_state = sntpc_sm_init;
+                return false;
+            }
+
+            // write data to the packet
+            _udp->write(_data_out.raw, sizeof(ntp_packet_t));
+
+            // advance if successful
+            _sm_state = sntpc_sm_send_packet;
+            break;
+
+        // send the packet off
+        case sntpc_sm_send_packet:
+            ret = _udp->endPacket();
+            if(ret == 0)
+            {
+                // failed reset to init
+                _sm_state = sntpc_sm_init;
+                return false;
+            }
+
+            // advance
+            _sm_state = sntpc_sm_await_response;
+            break;
+
+        // wait for the sm to reply
+        case sntpc_sm_await_response:
+            if(_udp->parsePacket() >= sizeof(ntp_packet_t))
+            {
+                // packet went through and we got a response
+                _sm_state = snptc_sm_read_response;
+            }
+            if(_fcount > SNTPC_FCOUNT)
+            {
+                // waited too long
+                _sm_state = sntpc_sm_init; 
+            }
+            break;
+        
+        // read the response in to the in buf
+        case snptc_sm_read_response:
+            if(_udp->read(_data_in.raw, sizeof(ntp_packet_t)) < sizeof(ntp_packet_t))
+            {
+                // reset
+                _sm_state = sntpc_sm_init;
+                return false;
+            }
+            break;
+
+        // decode the data
+        case snptc_sm_decode:
+            // nothing to do for now, reserved for future versions of ntp
+            // reset and return good news
+            _sm_state = sntpc_sm_init;
+            return true;
+
+        default:
+            // reset sm
+            _sm_state = sntpc_sm_init;
+            break;
+    }
+    return false;
+}
---
+++ src/libs/SNTPClient/SNTPClient.h
@@ -0,0 +1,96 @@
+#ifndef SNTP_CLIENT_H
+#define SNTP_CLIENT_H
+
+// local includes
+#include "Arduino.h"
+#include "Udp.h"
+
+
+// local defines
+#define SNTPC_DEFAULT_SERVER        pool.ntp.org
+#define SNTPC_PORT                  123
+#define SNTPC_SERVER_NAME_MAX_LEN   0x40;
+#define SNTPC_FCOUNT                50
+
+// not to be used with parallel udp streams, call to getNTPtime flushes the udp buffers. use with caution
+
+class SNTPClient
+{
+    // ntp packet stuct RFC5905: https://www.ietf.org/rfc/rfc5905.txt
+    #pragma pack(push,1)
+    typedef struct
+    {
+        uint8_t li:2;                 // leep indicator
+        uint8_t vn:3;                 // version number
+        uint8_t mode:3;               // mode
+
+        uint8_t stratum;              // stratum level of the local clock
+        int8_t poll;                 // maximum interval between successive messages
+        int8_t precision;            // precision of the local clock
+
+        uint32_t root_delay;          // total round trip delay time
+        uint32_t root_dispersion;     // max error aloud from primary clock source
+        uint32_t ref_Id;              // reference clock identifier
+
+        uint32_t ref_ts_s;            // reference time-stamp seconds
+        uint32_t ref_ts__f;           // reference time-stamp fraction of a second
+
+        uint32_t orig_ts_s;           // originate time-stamp seconds
+        uint32_t orig_ts_f;           // originate time-stamp fraction of a second
+
+        uint32_t rx_ts_s;             // received time-stamp seconds
+        uint32_t rx_ts_f;             // received time-stamp fraction of a second
+
+        uint32_t tx_ts_s;             // transmit time-stamp seconds
+        uint32_t tx_ts_f;             // transmit time-stamp fraction of a second
+
+    } ntp_packet_t;
+    #pragma pack(pop)
+
+    typedef union
+    {
+    ntp_packet_t ntp_pkt;
+    uint8_t raw[sizeof(ntp_packet_t)];
+    }ntp_stream_t;
+
+    typedef enum
+    {
+        sntpc_sm_init,
+        sntpc_sm_flush_udp,
+        sntpc_sm_begin_pkt,
+        sntpc_sm_send_packet,
+        sntpc_sm_await_response,
+        snptc_sm_read_response,
+        snptc_sm_decode,
+    }sntpc_sm_t;
+
+    typedef union
+    {
+        uint32_t i32u[2];
+        uint64_t i64u;
+    }i32u2i64u_t;
+
+
+    public:
+        static SNTPClient& getInstance();
+        ~SNTPClient();
+        bool getNTPtime();
+        void init(UDP* udp_inst, const char* server_name = SNTPC_DEFAULT_SERVER, uint64_t time_offset = 0);
+        void setServerName(const char* name);
+        void setServerIP(IPAddress ip);
+
+
+    private:
+        SNTPClient();
+        ntp_stream_t    _data_out; 
+        ntp_stream_t    _data_in; 
+        UDP*            _udp;
+        char*           _pool_name[SNTPC_SERVER_NAME_MAX_LEN];
+        IPAddress       _pool_ip;
+        bool            _initialized;
+        bool            _use_ip = false;
+        sntpc_sm_t      _sm_state;
+        uint16_t        _fcount;
+};
+
+#endif
